04d1f5d304902ac29539908a87e991944865f212
Simón Gutiérrez Brida <simon.gutierrez.brida@gmail.com>
2023-12-04 14:59:28 +0800

    Added comments

--- tutorials/tutorial9/length_test.s
+++ tutorials/tutorial9/length_test.s
@@ -4,30 +4,29 @@
 .text
 
 main:
-    push %rbp
-    mov %rsp, %rbp
-    
+    push %rbp               # Function prologue
+    mov %rsp, %rbp          # Function prologue
     # assume the string is not null
-    mov $0, %rsi
+    mov $0, %rsi            # we will use rsi as an index and a counter
     jmp str_length_loop_test
 
 str_length_loop_body:
-    inc %rsi
+    inc %rsi                # Increases the index / counter by 1
     
 str_length_loop_test:
-    mov $my_string, %rdi
-    mov (%rdi, %rsi), %cl
-    cmp $0, %cl
-    jnz str_length_loop_body
+    mov $my_string, %rdi    # moves the address of my_string, not the value pointed at by that address to rdi
+    mov (%rdi, %rsi), %cl   # moves 1 Byte (given by the use of %cl as destination) at address rdi + rsi
+    cmp $0, %cl             # does cl - 0 without modifying any operand
+    jnz str_length_loop_body    # if the previous operation resulted in 0, the Zero Flag will be 1, this will jump if ZF is 0
     
-    xor %rax, %rax
-    mov $result_format, %rdi
-    mov %rsi, %rdx
-    mov $my_string, %rsi
-    call printf
-    leave
-    mov $0, %rax
-    ret
+    xor %rax, %rax              # ensures rax is all zeros, to avoid missbehavior of printf
+    mov $result_format, %rdi    # sets the address of the first byte of the format string as first argument (the "pointer" to a string)
+    mov %rsi, %rdx              # sets the index / counter value to the third argument of printf
+    mov $my_string, %rsi        # sets the address of the first byte of the "Hello World!" string as the second argument of printf
+    call printf                 # calls printf
+    leave                       # Function epilogue
+    mov $0, %rax                # moves 0 to rax, so the main function can return 0
+    ret                         # returns to the previous function (this would be function _start from the C standard library)
     
        
 .data
--- tutorials/tutorial9/upper_test.s
+++ tutorials/tutorial9/upper_test.s
@@ -6,26 +6,27 @@
 main:
     push %rbp
     mov %rsp, %rbp
-    push $0 # size in -8(%rbp)
+    push $0 # size/index is in -8(%rbp)
     jmp str_length_loop_test
 
 str_length_loop_body:
-    incq -8(%rbp)
+    incq -8(%rbp)   # we need to specify that we are increment an 8 Bytes value, because the destination is a memory address
     
 str_length_loop_test:
-    mov $my_string, %rdi
-    mov -8(%rbp), %rsi
-    mov (%rdi, %rsi), %cl
-    cmp $0, %cl
-    jnz str_length_loop_body
+    mov $my_string, %rdi    # moves the address of our "Hello World!" string to rdi
+    mov -8(%rbp), %rsi      # moves size/index to rsi 
+    mov (%rdi, %rsi), %cl   # moves the byte (character) at rdi+rsi ("Hello World!"[rsi]) to cl
+    cmp $0, %cl             # compares the character obtained with 0
+    jnz str_length_loop_body    # goes back to the loop if the character is not 0
     
-    mov -8(%rbp), %rdi
-    inc %rdi
-    call malloc # we will assume malloc succeded
-    push %rax # upper is at -16(%rbp)
-    mov -8(%rbp), %r8
-    movb $0, (%rax, %r8)
-    push $0 # i is at -24(%rbp)
+    mov -8(%rbp), %rdi      # moves size/index to rdi (the first argument for malloc)
+    inc %rdi                # increases the size by 1, to accomodate for the ending 0
+    call malloc             # we call malloc and we will assume malloc succeded
+    push %rax               # the pointer/address to the upper string version is at -16(%rbp)
+    mov -8(%rbp), %r8       # we move the pointer/address of upper to r8
+    movb $0, (%rax, %r8)    # we put a 0 at upper[size] or upper+size
+    push $0                 # we save the index i with 0 at -24(%rbp)
+    # TODO: complete implementation of to_upper
     
        
 .data
